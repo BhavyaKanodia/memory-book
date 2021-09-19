9e85771dd86558caca22d5adc38bb99bc4820c0d
BhavyaKanodia <kanodiabhavya@gmail.com>
2021-09-19 18:09:20 +0530

    Made client side jsx structure and added styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import memoryBook from './images/memorybook.png';
+import Form from './components/Form/Form';
+import Posts from './components/Posts/Posts';
+import useStyles from './styles';
 
 const App = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memory Book
+        </Typography>
+        <img
+          className={classes.img}
+          src={memoryBook}
+          alt="memory book"
+          height="60"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
